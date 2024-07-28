871d38f10eea84e9d614591af9972e3623768005
Juanita <114871036+JuanitaCathy@users.noreply.github.com>
2024-07-28 09:26:54 +0000

    Setup convex, clerk and openai. created basic layouts of all pages

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# notes
+13:26 in the video check out for style
+
+globals.css and tailwind.config.ts la u gotta do sme chamges
+
+okay da
+i will see
+gimme few mins
+kayy bb
+
+logos and stufff and icons ill skip, 31.13 time videom zip 
+
+style i opy pasted time being will change later\
+
+logo icon in  main layout.tsx shd be changed later
---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import ConvexClerkProvider from "./providers/ConvexClerkProvider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "AudioNotes AI",
+  description: "eyyyy",
+  icons: {
+    icon: '/logoicon'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+      <ConvexClerkProvider>{children}</ConvexClerkProvider>
+      </body>
+    </html>
+  );
+}
